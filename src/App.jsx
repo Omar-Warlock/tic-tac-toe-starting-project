3e0f7a2042b691a84d13d364105e924e1f59a48f
OMR <cartoon6676@gmail.com>
2026-03-15 23:17:46 +0200

    added player logic and name handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import Player from "./components/Player";
 
 function App() {
-  
-
+  const [firstPlayerName, setFirstPlayerName] = useState("Player 1");
+  const [secondPlayerName, setSecondPlayerName] = useState("Player 2");
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player playerName={"Omar"} playerSymbol={"X"} />
+          <Player playerName={"Magdi"} playerSymbol={"O"} />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function Player({ playerName: pName, playerSymbol }) {
+  const [editing, setEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(pName);
+  return (
+    <li>
+      <span className="player">
+        {editing ? (
+          <input
+            type="text"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{playerSymbol}</span>
+      </span>
+      <button onClick={() => setEditing((edit) => !edit)}>
+        {editing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
+
+export default Player;
